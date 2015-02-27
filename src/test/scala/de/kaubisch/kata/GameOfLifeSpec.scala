9cbc5a399203f37afc08b689a4a683ebb3910757
Andreas Kaubisch <andreas.kaubisch@gmail.com>
2015-02-27 22:32:45 +0100

    make asserts more readable

--- src/test/scala/de/kaubisch/kata/GameOfLifeSpec.scala
+++ src/test/scala/de/kaubisch/kata/GameOfLifeSpec.scala
@@ -7,14 +7,14 @@ class GameOfLifeSpec extends FlatSpec with Matchers {
   "Game of Life" should "has one alive cell when one cell is placed" in {
     GameOfLife()
       .addCell(Alive(1, 1))
-      .hasCell(Alive(1, 1)) should be === true
+      .hasCell(Alive(1, 1)) shouldBe true
   }
 
   it should "kill an alive cell after next turn" in {
     GameOfLife()
       .addCell(Alive(1, 1))
       .makeTurn
-      .hasCell(Dead(1,1)) should be === true
+      .hasCell(Dead(1,1)) shouldBe true
   }
 
   it should "keep alive a cell after next turn when it has 2 neighbours" in {
@@ -23,7 +23,7 @@ class GameOfLifeSpec extends FlatSpec with Matchers {
       .addCell(Alive(2, 1))
       .addCell(Alive(1, 2))
       .makeTurn
-      .hasCell(Alive(1, 1)) should be === true
+      .hasCell(Alive(1, 1)) shouldBe true
   }
 
   it should "keep alive a cell after next turn when it has 3 neighbours" in {
@@ -33,7 +33,7 @@ class GameOfLifeSpec extends FlatSpec with Matchers {
       .addCell(Alive(1, 2))
       .addCell(Alive(0, 1))
       .makeTurn
-      .hasCell(Alive(1,1)) should be === true
+      .hasCell(Alive(1,1)) shouldBe true
   }
 
   it should "kills an alive cell after net turn when it has more then 3 neighbours" in {
@@ -44,7 +44,7 @@ class GameOfLifeSpec extends FlatSpec with Matchers {
       .addCell(Alive(0, 1))
       .addCell(Alive(0, 2))
       .makeTurn
-      .hasCell(Dead(1,1)) should be === true
+      .hasCell(Dead(1,1)) shouldBe true
   }
 
   it should "reborn a dead cell after next turn when it has exact 3 neighbours" in {
@@ -54,7 +54,7 @@ class GameOfLifeSpec extends FlatSpec with Matchers {
       .addCell(Alive(1,2))
       .addCell(Dead(2,2))
       .makeTurn
-      .hasCell(Alive(2,2)) should be === true
+      .hasCell(Alive(2,2)) shouldBe true
   }
 
   it should "create a blinker after next turn when it has correct cells set" in {
@@ -66,9 +66,9 @@ class GameOfLifeSpec extends FlatSpec with Matchers {
       .addCell(Dead(1,-1))
       .makeTurn
 
-      game.hasCell(Alive(1, 1)) should be === true
-      game.hasCell(Alive(1,-1)) should be === true
-      game.hasCell(Dead(0,0)) should be === true
-      game.hasCell(Dead(2,0)) should be === true
+      game.hasCell(Alive(1, 1)) shouldBe true
+      game.hasCell(Alive(1,-1)) shouldBe true
+      game.hasCell(Dead(0,0)) shouldBe true
+      game.hasCell(Dead(2,0)) shouldBe true
   }
 }
